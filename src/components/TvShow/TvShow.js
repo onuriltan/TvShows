0decdc5591f2b4d5db4b281ca1bcdc8ebc881c86
onuriltan <onur.iltan@gmail.com>
2019-05-15 16:47:02 +0300

    media queries added

--- src/components/TvShow/TvShow.js
+++ src/components/TvShow/TvShow.js
@@ -6,11 +6,14 @@ export default (data) => {
   }
 
   const {name, image, summary, type, url, premiered, genres} = data;
-console.log(image)
+  console.log(image)
   return (
       <div className="show_details">
-        <div className="show_details__img">
-          <img className="show_details__img__img" src={image.original} alt={name} />
+        <div className="show_details__img-container">
+          <img className="show_details__img-container__img" src={image.original} alt={name}/>
+        </div>
+        <div className="show_details__container">
+          <div className="show_details__container__name">{name}</div>
         </div>
       </div>
   );
