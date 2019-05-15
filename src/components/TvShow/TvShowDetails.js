e5926dd033799ad56832817da1109865bef1cfd5
onuriltan <onur.iltan@gmail.com>
2019-05-15 22:38:00 +0300

    add null or undefined check for conditional render details

--- src/components/TvShow/TvShowDetails.js
+++ src/components/TvShow/TvShowDetails.js
@@ -3,18 +3,37 @@ import React from 'react';
 export default ({network, rating, premiered, genres, language}) => {
   return (
       <div className="show_details">
-        <div className="show_details__detail">
-          Premiered : {premiered}
-        </div>
-        <div className="show_details__detail">
-          Genres : {genres.map((genre, i) =>
-            i + 1 === genres.length ? genre : genre + ", "
-        )}
-        </div>
-        <div className="show_details__detail">Language : {language}</div>
-        <div className="show_details__detail">Rating : {rating.average}</div>
-        <div className="show_details__detail">Country : {network.country.name}</div>
+        { premiered
+            ?
+            <div className="show_details__detail">
+              Premiered : {premiered}
+            </div>
+            : null
+        }
+        { genres
+            ?
+            <div className="show_details__detail">
+              Genres : {genres.map((genre, i) =>
+                i + 1 === genres.length ? genre : genre + ", "
+            )}
+            </div>
+            : null
+        }
+        { language
+            ?
+            <div className="show_details__detail">Language : {language}</div>
+            : null
+        }
+        { rating.average !== undefined && rating.average !== null
+            ?
+            <div className="show_details__detail">Rating : {rating.average}</div>
+            : null
+        }
+        { network !== undefined && network !== null
+            ?
+            <div className="show_details__detail">Country : {network.country.name}</div>
+            : null
+        }
       </div>
-
   );
 };
